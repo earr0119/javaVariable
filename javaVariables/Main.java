036d0f0f83992b1860e2b2bd2b733af6f33d656d
unknown <earr0119@hotmail.com>
2022-09-14 20:37:42 -0400

    first commit in Variables

---
+++ javaVariables/Main.java
@@ -0,0 +1,20 @@
+package javaVariables;
+
+public class Main {
+
+	public static void main(String[] args) {
+	  bolean decision = true;
+	  long = 925064025L;
+	  int = 24586;
+	  short = -32;
+	  byte = 120;
+	  double 320;
+	  float = 31f;
+	  char caracter = '4';
+	  string word = "hello world";
+			  
+	  
+	
+	
+	}
+}
